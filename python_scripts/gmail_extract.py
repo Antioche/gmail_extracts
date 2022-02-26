bd92bd46cda274b1fb7385fd26b4165322fbe1d3
Antioche <anne.tixier@pm.me>
2022-02-26 18:31:44 +1300

    First Full Version

--- python_scripts/gmail_extract.py
+++ python_scripts/gmail_extract.py
@@ -1,10 +1,14 @@
 from imap_tools import MailBox
 
 # get all attachments from INBOX and save them to files
-with MailBox('imap.gmail.com').login('tixieran', '#d!T8=dgfx6WS#@Ty7y0d', 'INBOX') as mailbox:
+with MailBox('imap.gmail.com').login('tixieran@gmail.com', "Fq0eNK7@BCD&I", 'INBOX') as mailbox:
     for msg in mailbox.fetch():
-        print(msg.subject)
+        #print(msg.subject)
         for att in msg.attachments:
-            print('-', att.filename, att.content_type)
-            with open('C:/1/{}'.format(att.filename), 'wb') as f:
-                f.write(att.payload)
+            #print('-', att.filename, att.content_type)
+            attach_name = '/home/tixiera/projects/gmail_extracts/attachments/' + format(att.filename.strip())
+            print(attach_name)
+            if attach_name != '/home/tixiera/projects/gmail_extracts/attachments/':
+                #with open('/home/tixiera/projects/gmail_extracts/attachments/{}'.format(att.filename), 'wb') as f:
+                with open(attach_name, 'wb') as f:
+                    f.write(att.payload)
