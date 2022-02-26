5748e71f2dca7f5828b6aa878dc63f370e5b0952
Antioche <anne.tixier@pm.me>
2022-02-26 23:28:20 +1300

    Version with hardcoded address, password, date low, date high

--- python_scripts/gmail_extract.py
+++ python_scripts/gmail_extract.py
@@ -1,14 +1,28 @@
 from imap_tools import MailBox
+import os
+
+address = 'tixieran@gmail.com'
+password = 'Fq0eNK7@BCD&I'
+date_low = 201801
+date_high = 201802
 
 # get all attachments from INBOX and save them to files
-with MailBox('imap.gmail.com').login('tixieran@gmail.com', "Fq0eNK7@BCD&I", 'INBOX') as mailbox:
+with MailBox('imap.gmail.com').login(address, password, 'INBOX') as mailbox:
     for msg in mailbox.fetch():
+        message_date = str(msg.date.year) + str(msg.date.month).rjust(2,'0')
+        message_date_int = int(message_date)
         #print(msg.subject)
+        if message_date_int >= date_high:
+            break
         for att in msg.attachments:
-            #print('-', att.filename, att.content_type)
-            attach_name = '/home/tixiera/projects/gmail_extracts/attachments/' + format(att.filename.strip())
-            print(attach_name)
-            if attach_name != '/home/tixiera/projects/gmail_extracts/attachments/':
-                #with open('/home/tixiera/projects/gmail_extracts/attachments/{}'.format(att.filename), 'wb') as f:
-                with open(attach_name, 'wb') as f:
-                    f.write(att.payload)
+            if message_date_int >= date_low and message_date_int < date_high:
+                attach_write_path = '/home/tixiera/projects/gmail_extracts/attachments/' + message_date + '/'
+                attach_write_name = attach_write_path + format(att.filename.strip())
+                #print(message_date)
+                if not os.path.exists(attach_write_path):
+                    print ('path ' + attach_write_path + 'does not exist. trying to make')
+                    os.makedirs(attach_write_path)
+                if attach_write_name != attach_write_path:
+                    print('Saving ' + attach_write_name)
+                    with open(attach_write_name, 'wb') as f:
+                        f.write(att.payload)
