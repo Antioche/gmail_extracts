36c58eb16fd805c3a6b519e218e0a83a470fb383
Antioche <anne.tixier@pm.me>
2022-02-26 23:30:26 +1300

    Made anonymous

--- python_scripts/gmail_extract.py
+++ python_scripts/gmail_extract.py
@@ -1,7 +1,7 @@
 from imap_tools import MailBox
 
 # get all attachments from INBOX and save them to files
-with MailBox('imap.gmail.com').login('tixieran@gmail.com', "Fq0eNK7@BCD&I", 'INBOX') as mailbox:
+with MailBox('imap.gmail.com').login('', "", 'INBOX') as mailbox:
     for msg in mailbox.fetch():
         #print(msg.subject)
         for att in msg.attachments:
